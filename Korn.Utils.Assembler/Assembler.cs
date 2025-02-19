3e93264a660cbce10b0968b93915a9e37d1adef8
Yotic <yoticcitoy@gmail.com>
2025-02-19 19:37:20 +0300

    Added disassembler and assembler. Added dependencies.

---
+++ Korn.Utils.Assembler/Assembler.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Korn.Utils.Assembler
+{
+    public unsafe struct Assembler
+    {
+        public Assembler(IntPtr* pointer) : this((byte**)pointer) { }
+        public Assembler(void** pointer) : this((byte**)pointer) { }
+        public Assembler(byte** pointer) => Pointer = pointer;
+
+        public byte** Pointer;
+
+        public Assembler WriteBytes(params byte[] bytes)
+        {
+            Memory.MemoryExtensions.Copy(*(IntPtr*)Pointer, bytes);
+            *Pointer += bytes.Length;
+
+            return this;
+        }
+
+        public Assembler MovRax64(long value)
+        {
+            write(0x48, 0xB8);
+            write64(value);
+
+            return this;
+        }
+
+        void write64(long value)
+        {
+            *(long*)*Pointer = value;
+            *Pointer += 8;
+        }
+
+        void write(params byte[] bytes)
+        {
+            for (var i = 0; i < bytes.Length; i++)
+                (*Pointer)[i] = bytes[i];
+            *Pointer += bytes.Length;
+        }
+
+        public static implicit operator Assembler(void** pcode) => new Assembler(pcode);
+        public static implicit operator void*(Assembler assembler) => assembler.Pointer;
+        public static implicit operator IntPtr(Assembler assembler) => (IntPtr)assembler.Pointer;
+    }
+}
---
+++ Korn.Utils.Assembler/Disassembler.cs
@@ -0,0 +1,23 @@
+﻿namespace Korn.Utils.Assembler
+{   
+    public unsafe class Disassembler
+    {
+        public static byte GetInstructionLength(byte* instruction) => GetInstructionLength(InstructionsTables.TABLE, instruction);
+
+        public static byte GetInstructionLength(byte[] table, byte* instruction)
+        {
+            byte value = table[*instruction++];
+            return value < 0x10 ? value : GetInstructionLength(InstructionsTables.TABLES[value - 0x10], instruction);
+        }
+
+        public static int CalculateMinInstructionLength(byte* instructions, int length)
+        {
+            int result = 0;
+
+            do result += GetInstructionLength(instructions + result);
+            while (result < length);
+
+            return result;
+        }
+    }
+}
